b3ccbaccecb3f08d79b5813949bcefa000f43dfc
lukeschula <schulaluke@gmail.com>
2024-05-02 11:12:46 -0500

    edit

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+  }
     var task = false;
     var task2 = true;
 
-    if (trueOrFalse) {
-      let task = true;
-      let task2 = false;
-    }
+
 
     return [task, task2];
   }
