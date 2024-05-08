b9445495a05ffe77c1083d3f60a95cfd05f52e73
lukeschula <schulaluke@gmail.com>
2024-05-08 14:56:01 -0500

    edit

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,3 +1,23 @@
 import Building from './5-building';
 
-export default class SkyHighBuilding
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof floors !== 'number') {
+      throw TypeError('Floors must be a number');
+    }
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
