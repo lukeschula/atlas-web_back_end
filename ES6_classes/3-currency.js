fdaa076c0e3064954a4b13030db491f8e9353f1a
lukeschula <schulaluke@gmail.com>
2024-05-08 14:16:49 -0500

    edit

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,11 +1,10 @@
-
 export default class Currency {
   constructor(code, name) {
     if (typeof code !== 'string') {
       throw TypeError('Code must be a string');
     }
     if (typeof name !== 'string') {
-        throw TypeError('Name must be a string');
+      throw TypeError('Name must be a string');
     }
     this._code = code;
     this._name = name;
