dda8821f30a7d790e3cae211eeca0d0bb299aa9b
lukeschula <schulaluke@gmail.com>
2024-05-08 19:09:19 -0500

    edit

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(newMap) {
   if (!(newMap instanceof Map)) {
     throw new TypeError('Cannot process');
   }
-  for (const [ item, quantity] of newMap) {
+  for (const [ item, quantity ] of newMap) {
     if (quantity === 1) {
       newMap.set(item, 100);
     }
