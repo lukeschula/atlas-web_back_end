8bc4505a21a980daeed0322e5d552ca5ba369709
lukeschula <schulaluke@gmail.com>
2024-08-27 10:30:13 -0500

    task 5 check

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""sum_list which takes a list input_list of floats"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ takes a list and finds the sum"""
+    return sum(input_list)
