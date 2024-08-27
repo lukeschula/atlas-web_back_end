4ad589185d32d0f69edc79a3903aaad533f11c01
lukeschula <schulaluke@gmail.com>
2024-08-27 10:10:29 -0500

    task 3 check

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+""" to_str that takes a float n as argument and returns """
+
+
+def to_str(n: float) -> str:
+    """takes a float and returns a string"""
+    return str(n)
