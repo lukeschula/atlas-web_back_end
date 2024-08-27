a5747c9e2a4d04d754daab0915b5e8dc109457ae
lukeschula <schulaluke@gmail.com>
2024-08-27 09:18:28 -0500

    edit

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,8 +2,8 @@
 
 ''' takes a float a and a float b as arguments and returns their sum as a float '''
 
+
 def add(a: float, b: float) -> float:
-    '''created function with a and b as parameter,
-    setting them as floats and returning a float after addition
-    '''
+    ''' created function with a and b as parameter,
+    setting them as floats and returning a float '''
     return a + b
