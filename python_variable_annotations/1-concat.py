38aabd0fac9777340d5242960e72fbbe72b4c40e
lukeschula <schulaluke@gmail.com>
2024-08-27 09:43:30 -0500

    task 1 done, i think

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" concat that takes a string str1 and a string str2 as arguments"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """take two strings and returns a string"""
+    return str1 + str2
