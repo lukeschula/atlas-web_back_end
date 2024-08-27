884f291d4b9aadeaaf3eb91799ffb13c1d2767ca
lukeschula <schulaluke@gmail.com>
2024-08-27 09:53:19 -0500

    edit

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+import math
 """ floor which takes a float n as argument """
 
 def floor(n: float) -> int:
     """n is a float to return int"""
-    return 3
+    int = 3
+    floor_return = math.floor(int)
+    return floor_return
 
